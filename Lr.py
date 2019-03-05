2a62ae81d788ac5cb73fa9faf66626291de5851a
zhuyanshuo <1240368588@qq.com>
2019-03-05 09:22:56 +0800

    Update Lr.py

--- Lr.py
+++ Lr.py
@@ -1,1 +1,14 @@
-print("zhuyasnhuo")
+# 导入相关数据包
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+%matplotlib inline
+
+from scipy import stats
+from scipy.stats import norm
+
+root_path = '/opt/data/kaggle/getting-started/house-prices'
+
+train = pd.read_csv('%s/%s' % (root_path, 'train.csv'))
+test = pd.read_csv('%s/%s' % (root_path, 'test.csv'))
